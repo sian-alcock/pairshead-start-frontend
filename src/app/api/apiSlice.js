ae02be919bca3ac891701283046bc1e82bed34cd
Sian Alcock <sian.alcock@lawena.org>
2022-10-22 15:21:38 +0100

    Fix base name

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://pairshead-start-api.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://pairshead-start.onrender.com' }),
     tagTypes: ['Crew', 'User'],
     // eslint-disable-next-line no-unused-vars
     endpoints: builder => ({})
